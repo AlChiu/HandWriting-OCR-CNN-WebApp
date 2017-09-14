9954c19400b70b24741a4bd4594325137a5733a7
Akshay Pawar <akshay070993@gmail.com>
2017-09-14 16:21:04 -0700

    Added function to convert image to grayscale (OpenCV)
    Added function to convert image to grayscale using Open cv and save it on the server.

--- back/app.py
+++ back/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
+import cv2
 app=Flask(__name__)
 
 @app.route("/")
@@ -10,8 +11,13 @@ def main():
 def upload():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+        f.save(secure_filename(f.filename))  #Save image at the server
+        
+        image = cv2.imread(f.filename)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('gray_image/gray.jpeg', gray_image)   #save grayscale image at the server 
+        
+        return 'Image sucessfully converted to Grayscale'
         
 if __name__ == "__main__":
     app.run(debug = True)
