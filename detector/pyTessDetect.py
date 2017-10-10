2cf4cefe17f2bdc37ffad8ddd75e5ccd905e6a73
Alexander <al.chiu407@gmail.com>
2017-10-10 15:22:55 -0700

    Detector will crop images of words that it detects

--- detector/pyTessDetect.py
+++ detector/pyTessDetect.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 from PIL import Image
-from pytesseract import pytesseract
+from tesserocr import PyTessBaseAPI, RIL
+import numpy
 import argparse
 import csv
 import cv2
@@ -20,7 +21,6 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Threshold the image
 if args["preprocess"] == "thresh":
   gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-  gray = cv2.GaussianBlur(gray, (5,5), 0)
 
 # Blur if preprocess has bluring
 elif args["preprocess"] == "blur":
@@ -30,14 +30,52 @@ elif args["preprocess"] == "blur":
 filename = "{}.png".format(os.getpid())
 cv2.imwrite(filename, gray)
 
-pytesseract.run_tesseract(filename, 'output', lang=None, boxes=True, config="hocr")
+with PyTessBaseAPI() as api:
+  # Send the new grayscale image into Tesseract
+  api.SetImageFile(filename)
 
-# Load the image as a PIL image, OCR, then delete
-text = pytesseract.image_to_string(Image.open(filename))
+  # OCR the grayscale image for comparison
+  translation = api.GetUTF8Text()
+
+  # We will segment the grayscale by words
+  boxes = api.GetComponentImages(RIL.WORD, True)
+
+  # With each bouding box for the words
+  for i, (im, box, _, _) in enumerate(boxes):
+    # Grab the coordinates of the bounding box
+    api.SetRectangle(box['x'], box['y'], box['w'], box['h'])
+
+    # What is the word in the bounding box
+    ocrResult = api.GetUTF8Text()
+
+    # Get the confidence of its translation
+    conf = api.MeanTextConf()
+
+    # Turn the returned bounding box coordinates into an array of coordinates
+    coord = list(box.values())
+
+    # Load grayscale for cropping
+    cropper = Image.open(filename)
+
+    # Cropped image is saved into new variable
+    crop_image = cropper.crop(
+      (coord[0],
+      coord[1],
+      coord[0]+coord[2],
+      coord[1]+coord[3])
+    )
+
+    # Convert the new image into a numpy array
+    cropped = numpy.array(crop_image) 
+
+    # Create the new file name for the word
+    word_file = "word_" + str(i) + ".png"
+
+    # Have OpenCV save the cropped image into the new file
+    cv2.imwrite(word_file, cropped) 
+
+# Remove the grayscale image
 os.remove(filename)
-print(text)
 
-# show the output images
-cv2.imshow("Image", gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Print out Tessearct's translation for comparison
+print(translation)
