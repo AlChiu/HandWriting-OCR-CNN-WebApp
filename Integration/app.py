57159ad6cb5c261172257e5d10157d4fe3daa02b
Alexander <al.chiu407@gmail.com>
2017-10-14 14:17:42 -0700

    Begin ingrating detector, web front, and classifier.

---
+++ Integration/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+import cv2
+import detector
+import classifier
+app=Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))  #Save image at the server
+        
+        detector.detector(f.filename)
+        words = classifier.classify()
+        
+        return words
+        
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ Integration/classifier.py
@@ -0,0 +1,19 @@
+import glob
+import re
+from dataload import WordClassifier
+
+numbers = re.compile(r'(\d+)')
+def numericalSort(value):
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
+
+def classify():
+  classifier = WordClassifier(modelPath='model-1000word.h5')
+
+  words = []
+
+  for image in sorted(glob.iglob('word_*.png'), key=numericalSort):
+    words.append(classifier.classify_image(image))
+
+  return words
